8a2d7b0af8a5553661b4bb124e4023bfb10c43c3
Renming Zhang <kevin110299@126.com>
2022-10-07 17:33:14 -0400

    pytest added

--- skeleton/tests.py
+++ skeleton/tests.py
@@ -1,48 +1,98 @@
-from .assignment_12 import *
+import pytest
+from assignment_12 import *
 
-sf=Scan(filepath="../data/friends.txt",outputs=None)
-sr=Scan(filepath="../data/movie_ratings.txt",outputs=None)
-se1=Select(inputs=[sf],predicate={"UID1":'1190'},outputs=None)
-se2=Select(inputs=[sr],predicate={"MID":'16015'},outputs=None)
-join=Join(left_inputs=[se1],right_inputs=[se2],outputs=None,left_join_attribute="UID2",right_join_attribute="UID")
-proj=Project(inputs=[join],outputs=None,fields_to_keep=["Rating"])
-groupby=GroupBy(inputs=[proj],outputs=None,key="",value="Rating",agg_gun="AVG")
-groupby.get_next()
+pathf="../data/friends.txt"
+pathr="../data/movie_ratings.txt"
+uid=10
+mid=3
+resPath="../data/res.txt"
+def test_query1_pull():
+    sf = Scan(filepath=pathf, outputs=None)
+    sr = Scan(filepath=pathr, outputs=None)
+    se1 = Select(inputs=[sf], predicate={"UID1": uid}, outputs=None)
+    se2 = Select(inputs=[sr], predicate={"MID": mid}, outputs=None)
+    join = Join(left_inputs=[se1], right_inputs=[se2], outputs=None, left_join_attribute="UID2",
+                right_join_attribute="UID")
+    proj = Project(inputs=[join], outputs=None, fields_to_keep=["Rating"])
+    groupby = GroupBy(inputs=[proj], outputs=None, key="", value="Rating", agg_gun="AVG")
+    sink=Sink(inputs=[groupby],outputs=None,filepath=resPath)
+    sink.get_next()
+    assert sink.output is not None
+    pass
 
-sf=Scan(filepath="../data/friends.txt",outputs=None)
-sr=Scan(filepath="../data/movie_ratings.txt",outputs=None)
-se1=Select(inputs=[sf],predicate={"UID1":'1190'},outputs=None)
-se2=Select(inputs=[sr],predicate=None,outputs=None)
-join=Join(left_inputs=[se1],right_inputs=[se2],outputs=None,left_join_attribute="UID2",right_join_attribute="UID")
-proj=Project(inputs=[join],outputs=None,fields_to_keep=["MID","Rating"])
-groupby=GroupBy(inputs=[proj],outputs=None,key="MID",value="Rating",agg_gun="AVG")
-orderby=OrderBy(inputs=[groupby],outputs=None,comparator="Rating",ASC=False)
-orderby.get_next()
+def test_query1_push():
+    sink = Sink(inputs=None, outputs=None, filepath=resPath)
+    groupby = GroupBy(inputs=None, outputs=[sink], key="", value="Rating", agg_gun="AVG")
+    proj = Project(inputs=None, outputs=[groupby], fields_to_keep=["Rating"])
+    join = Join(left_inputs=None, right_inputs=None, outputs=[proj], left_join_attribute="UID2",
+                right_join_attribute="UID")
+    se1 = Select(inputs=None, predicate={"UID1": uid}, outputs=[join])
+    sf = Scan(filepath=pathf, outputs=[se1])
+    se2 = Select(inputs=None, predicate={"MID": mid},outputs=[join])
+    sr = Scan(filepath=pathr, isleft=False, outputs=[se2])
+    sf.start()
+    sr.start()
+    assert sink.output is not None
+    pass
 
-sink= Sink()
-topk = TopK(inputs=None, outputs=[sink], k=1)
-orderby = OrderBy(inputs=None, outputs=[topk], comparator="Rating", ASC=False)
-gb = GroupBy(inputs=None, outputs=[orderby], key="MID", value="Rating", agg_gun="AVG")
-proj = Project(inputs=None, outputs=[gb], fields_to_keep=["MID", "Rating"])
-join = Join(left_inputs=None, right_inputs=None, outputs=[proj], left_join_attribute="UID2", right_join_attribute="UID")
-se1 = Select(inputs=None, predicate={"UID1": '1190'}, outputs=[join])
-sf = Scan(filepath="../data/friends.txt", outputs=[se1])
-se2 = Select(inputs=None, predicate=None, outputs=[join])
-sr = Scan(filepath="../data/movie_ratings.txt", isleft=False, outputs=[se2])
-sf.start()
-sr.start()
+def test_query2_pull():
+    sf = Scan(filepath=pathf, outputs=None)
+    sr = Scan(filepath=pathr, outputs=None)
+    se1 = Select(inputs=[sf], predicate={"UID1": uid}, outputs=None)
+    se2 = Select(inputs=[sr], predicate=None, outputs=None)
+    join = Join(left_inputs=[se1], right_inputs=[se2], outputs=None, left_join_attribute="UID2",
+                right_join_attribute="UID")
+    proj = Project(inputs=[join], outputs=None, fields_to_keep=["MID", "Rating"])
+    groupby = GroupBy(inputs=[proj], outputs=None, key="MID", value="Rating", agg_gun="AVG")
+    orderby = OrderBy(inputs=[groupby], outputs=None, comparator="Rating", ASC=False)
+    topk = TopK(inputs=[orderby], outputs=None, k=1)
+    sink = Sink(inputs=[topk], outputs=None, filepath=resPath)
+    sink.get_next()
+    assert sink.output is not None
+    pass
 
+def test_query2_push():
+    sink = Sink(inputs=None, outputs=None, filepath=resPath)
+    topk = TopK(inputs=None, outputs=[sink], k=1)
+    orderby = OrderBy(inputs=None, outputs=[topk], comparator="Rating", ASC=False)
+    gb = GroupBy(inputs=None, outputs=[orderby], key="MID", value="Rating", agg_gun="AVG")
+    proj = Project(inputs=None, outputs=[gb], fields_to_keep=["MID", "Rating"])
+    join = Join(left_inputs=None, right_inputs=None, outputs=[proj], left_join_attribute="UID2",
+                right_join_attribute="UID")
+    se1 = Select(inputs=None, predicate={"UID1": uid}, outputs=[join])
+    sf = Scan(filepath=pathf, outputs=[se1])
+    se2 = Select(inputs=None, predicate=None, outputs=[join])
+    sr = Scan(filepath=pathr, isleft=False, outputs=[se2])
+    sf.start()
+    sr.start()
+    assert sink.output is not None
+    pass
 
-hist=Histogram(inputs=None,outputs=None)
-proj = Project(inputs=None, outputs=[hist], fields_to_keep=["Rating"])
-join = Join(left_inputs=None, right_inputs=None, outputs=[proj], left_join_attribute="UID2", right_join_attribute="UID")
-se1 = Select(inputs=None, predicate={"UID1": '1190'}, outputs=[join])
-sf = Scan(filepath="../data/friends.txt", outputs=[se1])
-se2 = Select(inputs=None, predicate=None, outputs=[join])
-sr = Scan(filepath="../data/movie_ratings.txt", isleft=False, outputs=[se2])
-sf.start()
-sr.start()
+def test_query3_pull():
+    sf = Scan(filepath=pathf, outputs=None)
+    sr = Scan(filepath=pathr, outputs=None)
+    se1 = Select(inputs=[sf], predicate={"UID1": uid}, outputs=None)
+    se2 = Select(inputs=[sr], predicate={"MID": mid}, outputs=None)
+    join = Join(left_inputs=[se1], right_inputs=[se2], outputs=None, left_join_attribute="UID2",
+                right_join_attribute="UID")
+    proj = Project(inputs=[join], outputs=None, fields_to_keep=["Rating"])
+    hist = Histogram(inputs=[proj], outputs=None)
+    sink = Sink(inputs=[hist], outputs=None, filepath=resPath)
+    sink.get_next()
+    assert sink.output is not None
+    pass
 
-query1(False,"../data/friends.txt","../data/movie_ratings.txt",1190,16015,"../data/res.txt")
-query2(False,"../data/friends.txt","../data/movie_ratings.txt",1190,None,"../data/res.txt")
-query3(True,"../data/friends.txt","../data/movie_ratings.txt",1190,16015,"../data/res.txt")
+def test_query3_push():
+    sink = Sink(inputs=None, outputs=None, filepath=resPath)
+    hist = Histogram(inputs=None, outputs=[sink])
+    proj = Project(inputs=None, outputs=[hist], fields_to_keep=["Rating"])
+    join = Join(left_inputs=None, right_inputs=None, outputs=[proj], left_join_attribute="UID2",
+                right_join_attribute="UID")
+    se1 = Select(inputs=None, predicate={"UID1": uid}, outputs=[join])
+    sf = Scan(filepath=pathf, outputs=[se1])
+    se2 = Select(inputs=None, predicate={"MID": mid},outputs=[join])
+    sr = Scan(filepath=pathr, isleft=False, outputs=[se2])
+    sf.start()
+    sr.start()
+    assert sink.output is not None
+    pass
